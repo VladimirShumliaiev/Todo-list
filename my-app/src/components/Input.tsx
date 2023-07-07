3b0a107449302120a79c5190b15ec2135fc949cc
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-07-07 22:03:35 +0300

    Todo-list + react-paginate

--- my-app/src/components/Input.tsx
+++ my-app/src/components/Input.tsx
@@ -26,7 +26,7 @@ const Input: FC<InputProps> = (props) => {
                 ref={inputRef}
                 placeholder={'add text...'}
             />
-            <button>add text</button>
+            <button>add</button>
         </form>
     );
 };
