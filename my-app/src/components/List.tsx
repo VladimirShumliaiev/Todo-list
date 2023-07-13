f3d7f1a1452377d21634484d27d010f50d2f477d
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-07-13 22:58:24 +0300

    Todo-list + react-paginate

--- my-app/src/components/List.tsx
+++ my-app/src/components/List.tsx
@@ -9,7 +9,7 @@ const List = () => {
     const [currentItems, setCurrentItems] = useState(ListSelector);
     const [pageCount, setPageCount] = useState(0);
     const [itemOffset, setItemOffset] = useState(0);
-    const itemsPerPage = 8
+    const itemsPerPage = 7
 
     useEffect(() => {
         const endOffset = itemOffset + itemsPerPage;
