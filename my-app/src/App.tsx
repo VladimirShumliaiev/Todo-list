99db8c4ce562a127ea02f9562ac11b797dd10d00
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-10-28 12:42:09 +0300

    Todo-list +

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,29 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import Input from "./components/Input";
-import List from "./components/List";
-import {useAppDispatch, useAppSelector} from "./hooks/hooks";
-import {addTodo, fetchTodo} from "./ReduxTK/slices/todoSlice";
 import './App.css';
 
 const App = () => {
     const [theme, setTheme] = useState('light')
-    const [title, setTitle] = useState('')
-    const {error, pending} = useAppSelector(state => state.todo)
-    const dispatch = useAppDispatch()
 
     useEffect(() => {
         document.body.setAttribute('theme-data', theme)
     })
-
-
-    useEffect(() => {
-        dispatch(fetchTodo())
-    }, [])
-
-    const addTask = () => {
-        dispatch(addTodo(title))
-    }
-
 
     const handleThem = () => {
         setTheme(theme === 'light' ? 'dark' : 'light')
@@ -35,12 +18,9 @@ const App = () => {
                 <button onClick={handleThem}>theme</button>
             </div>
             <div className={'headerL'}>
-                <Input title={title} setTitle={setTitle} addTodo={addTask}/>
             </div>
             <div className={'n'}>
-                {pending && <h3>Loading...</h3>}
-                {error && <h3>{error}</h3>}
-                <List/>
+             f
             </div>
         </div>
     );
