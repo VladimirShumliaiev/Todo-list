ba66044dbd97cbd72eb3417a63441d696e05d0f7
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-01-15 20:57:32 +0200

    Todo-list +

--- my-app/src/components/Item.tsx
+++ my-app/src/components/Item.tsx
@@ -4,29 +4,31 @@ import {removeTodo, toggleTodo} from "../ReduxTK/slices/todoSlice";
 
 type ItemProps = {
     title: string
-    completed: boolean
     id: string
+    completed: boolean
+
 }
 
-const Item:FC<ItemProps> = (props) => {
-    const {title,completed,id} = props
+const Item: FC<ItemProps> = (props) => {
+    const {title, completed, id} = props
     const dispatch = useAppDispatch()
 
-    const onChangeHandle: React.ChangeEventHandler<HTMLInputElement> = () => {
+    const onChangeHandle: React.ChangeEventHandler<HTMLInputElement>  = () => {
         dispatch(toggleTodo(id))
     }
 
     const onClickHandle: React.MouseEventHandler<HTMLButtonElement> = () => {
-        if (window.confirm('do you want to delete todo?')) {
+        if (window.confirm('delete todo?')){
             dispatch(removeTodo(id))
         }
+
     }
 
     return (
         <div>
-            <input type={"checkbox"} checked={completed} onChange={onChangeHandle}/>
+            <input type="checkbox" checked={completed} onChange={onChangeHandle}/>
             {title}
-            <button onClick={onClickHandle}>delete</button>
+            <button onClick={onClickHandle}>&times;</button>
         </div>
     );
 };
