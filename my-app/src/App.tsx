37502fc25ce4ca311cfbccdfb9be14bda3387714
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-01-18 19:55:37 +0200

    Todo-list +

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,47 +1,12 @@
-import React, {useEffect, useState} from 'react';
 import './App.css';
-import {useAppDispatch} from "./hooks/hooks";
-import {addTodo, fetchTodo} from "./ReduxTK/slices/todoSlice";
-import Input from "./components/Input";
-import List from "./components/List";
-
 
 const App = () => {
-    const [theme, setTheme] = useState('light')
-    const [title, setTitle] = useState('')
-    const dispatch = useAppDispatch()
-
-
-    useEffect(() => {
-        document.body.setAttribute('theme-data', theme)
-    })
-
-    const handleThem = () => {
-        setTheme(theme === 'light' ? 'dark' : 'light')
-    }
-
-    useEffect(() => {
-        dispatch(fetchTodo())
-    },[])
-
-    const addTask = () => {
-        dispatch(addTodo(title))
-    }
-
-    return (
-        <div className={'App'}>
-            <div className={'headerR'}>
-                <button onClick={handleThem}>theme</button>
-            </div>
-            <div className={'headerL'}>
-                Todos list:
-            </div>
-            <div className={'n'}>
-                <Input title={title} setTitle={setTitle} addTodo={addTask}/>
-                <List/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
 };
 
 export default App;
