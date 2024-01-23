ce7db6fd0883f02910ce35bf18cab0043e8d5557
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-01-23 22:43:09 +0200

    Todo-list +

--- my-app-vite/src/App.tsx
+++ my-app-vite/src/App.tsx
@@ -1,22 +1,11 @@
 import "./App.css"
-import { Counter } from "./features/counter/Counter"
-import { Quotes } from "./features/quotes/Quotes"
-import logo from "./logo.svg"
-import {useAppDispatch, useAppSelector} from "./app/hooks";
-import {fetchTodo, todoSelector} from "./redux/Slice/todoSlice";
-import {useEffect} from "react";
+
 
 const App = () => {
 
-  const dispatch = useAppDispatch()
-  const selector = useAppSelector(todoSelector)
-
-
   return (
     <div className="App">
-      {
-        selector.map(e => <div>{console.log(e.title)}</div>)
-      }
+        App
     </div>
   )
 }
