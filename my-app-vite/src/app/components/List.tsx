187f5a7cfcacf2c6f29e98e8adddb76dd90c70f2
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-01-30 18:53:12 +0200

    Todo-list +

--- my-app-vite/src/app/components/List.tsx
+++ my-app-vite/src/app/components/List.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {useAppSelector} from "../hooks";
+import {todoSelector} from "../../redux/Slice/todoSlice";
+import Item from "./Item";
 
 const List = () => {
+
+    const list = useAppSelector(todoSelector)
     return (
         <div>
-            Hi Hi
+            {
+                list.map(e =>  <Item key={e.title} {...e}/>)
+            }
         </div>
     );
 };
