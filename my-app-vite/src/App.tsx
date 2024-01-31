3af48fa7739130463863fc2c9c238ac23a9b5fc9
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-01-31 19:24:15 +0200

    Todo-list +

--- my-app-vite/src/App.tsx
+++ my-app-vite/src/App.tsx
@@ -3,17 +3,16 @@ import List from "./app/components/List";
 import {useEffect} from "react";
 import {useAppDispatch} from "./app/hooks";
 import {fetchTodo} from "./redux/Slice/todoSlice";
+import Button from "./app/components/Button";
 
 
 const App = () => {
     const dispatch = useAppDispatch()
-    useEffect(() => {
-        dispatch(fetchTodo())
-    },[])
 
   return (
     <div className="App">
-        <List/>
+        <Button/>
+        {/*<List/>*/}
     </div>
   )
 }
---
+++ my-app-vite/src/app/components/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <div>
+            <input type="text"/>
+            <button>push</button>
+        </div>
+    );
+};
+
+export default Button;
