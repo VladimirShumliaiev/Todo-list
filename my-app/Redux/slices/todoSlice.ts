a093693b1bdfc4ffcd4f3516bbdff5b455104d68
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-06-16 10:01:21 +0300

    Update todoSlice.ts

--- my-app/Redux/slices/todoSlice.ts
+++ my-app/Redux/slices/todoSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, isRejectedWithValue} from "@reduxjs/toolkit";
+import axios from 'axios'
 
 export type Todo = {
     id: string
@@ -17,6 +18,18 @@ const initialState: todoState = {
     error: null,
     pending: false,
 }
+
+export const todoFetch = createAsyncThunk<Todo[], undefined, {rejectValue: string}>(
+    'Todo/todoFetch',
+    async (_,{rejectWithValue}) => {
+        const response = await axios('https://jsonplaceholder.typicode.com/todos')
+
+        if (!response) {
+            return rejectWithValue('error fetch todo')
+        }
+        return await response.data
+    }
+)
 
 const todoSlice = createSlice({
    name: 'Todo',
