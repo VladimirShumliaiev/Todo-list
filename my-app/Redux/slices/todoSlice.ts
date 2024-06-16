6a9ee46f900d241f32abd95879ec0dfb076cfda1
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-06-16 09:45:48 +0300

    Update todoSlice.ts

--- my-app/Redux/slices/todoSlice.ts
+++ my-app/Redux/slices/todoSlice.ts
@@ -1,9 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export type Todo = {
+    id: string
+    title: string
+    completed: boolean
+}
 
+type todoState = {
+    list: Todo[],
+    error: null | string
+    pending: boolean
+}
+
+const initialState: todoState = {
+    list: [],
+    error: null,
+    pending: false,
+}
 
 const todoSlice = createSlice({
-
+   name: 'Todo',
+   initialState,
+   reducers: {}
 })
 
 export default todoSlice.reducer
